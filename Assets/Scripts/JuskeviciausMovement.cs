556f28edd1c845c8225aaf69daf3c69f9a2daa07
Macbook Pro <mbp@Macbooks-MacBook-Pro.local>
2020-03-04 14:39:14 +0200

    THIC - 1 / THIC - 10 / THIC - 9
    * Fixed character movement and added animations ( now he can run left and right with animation).
    * Added enemys to the map.
    * Camera now follows main character.

---
+++ Assets/Scripts/JuskeviciausMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JuskeviciausMovement : MonoBehaviour
+{
+    
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")* 15f *Time.deltaTime, 0f, 0f);
+        Vector3 characterScale = transform.localScale;
+        if(Input.GetAxis("Horizontal") < 0){
+            characterScale.x = -1;
+        }
+        if(Input.GetAxis("Horizontal") > 0){
+            characterScale.x = 1;
+        }
+        transform.localScale = characterScale;
+    }
+}
