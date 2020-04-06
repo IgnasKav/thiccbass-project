96a087e5052127057381a5e7aa23a249c25186c0
IgnasKav <ignas.kavaliauskas5@gmail.com>
2020-04-06 23:07:51 +0300

    no-task
    Improving character movement

---
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField] private float m_JumpForce = 1100f;                         
+    [Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;    
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f; 
+    [SerializeField] private bool m_AirControl = false;                         
+    [SerializeField] private LayerMask m_WhatIsGround;                         
+    [SerializeField] private Transform m_CeilingCheck;                          
+    [SerializeField] private Collider2D m_CrouchDisableCollider;                
+
+    const float k_CeilingRadius = .2f; 
+    private Rigidbody2D rigidbody2D;
+    private BoxCollider2D boxCollider2D;
+    private bool m_FacingRight = true; 
+    private Vector3 m_Velocity = Vector3.zero;
+
+    [Header("Events")]
+    [Space]
+
+    public UnityEvent OnLandEvent;
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+
+    public BoolEvent OnCrouchEvent;
+    private bool m_wasCrouching = false;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+
+        if (OnCrouchEvent == null)
+            OnCrouchEvent = new BoolEvent();
+    }
+
+    private void FixedUpdate()
+    {
+        bool grounded = IsGrounded();
+        Debug.Log(grounded);
+    }
+
+    public bool IsGrounded() {
+        float extraHeight = 1f;
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, extraHeight, m_WhatIsGround);
+
+        return raycastHit.collider != null;
+    }
+
+
+    public void Move(float move, bool crouch, bool jump)
+    {
+        if (!crouch)
+        {
+            if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+            {
+                crouch = true;
+            }
+        }
+
+        if (IsGrounded() || m_AirControl)
+        {
+
+            if (crouch)
+            {
+                if (!m_wasCrouching)
+                {
+                    m_wasCrouching = true;
+                    OnCrouchEvent.Invoke(true);
+                }
+
+                move *= m_CrouchSpeed;
+
+                if (m_CrouchDisableCollider != null)
+                    m_CrouchDisableCollider.enabled = false;
+            }
+            else
+            {
+                if (m_CrouchDisableCollider != null)
+                    m_CrouchDisableCollider.enabled = true;
+
+                if (m_wasCrouching)
+                {
+                    m_wasCrouching = false;
+                    OnCrouchEvent.Invoke(false);
+                }
+            }
+
+            Vector3 targetVelocity = new Vector2(move * 10f, rigidbody2D.velocity.y);
+            rigidbody2D.velocity = Vector3.SmoothDamp(rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+            if (move > 0 && !m_FacingRight)
+            {
+                Flip();
+            }
+            else if (move < 0 && m_FacingRight)
+            {
+                Flip();
+            }
+        }
+        if (IsGrounded() && jump)
+        {
+            rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        }
+    }
+
+
+    private void Flip()
+    {
+        m_FacingRight = !m_FacingRight;
+        
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+    private Animator myAnimator;
+    public float runSpeed = 50f;
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+    void Start() {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        myAnimator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+            myAnimator.SetBool("isJumping", true);
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+	void FixedUpdate ()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        myAnimator.SetBool("isJumping", !controller.IsGrounded());
+		jump = false;
+	}
+}
