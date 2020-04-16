d3971da711785fe24a86af17c526c774bcdc3291
tomel1s <tomsin418@gmail.com>
2020-04-16 14:52:57 +0300

    THIC - 4/ THIC - 5
    Fixed enemy attack system.
    Enemys now have attack animation.

---
+++ Assets/Scripts/Enemy/BanditAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BanditAttack : MonoBehaviour
+{
+   
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name.Equals("Warrior"))
+        {
+            Bandit1.isAttacking = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.name.Equals("Warrior"))
+        {
+            Bandit1.isAttacking = false;
+        }
+    }
+}
