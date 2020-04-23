25afbd4c43f13e78cd0dc23d754124f128967983
IgnasKav <ignas.kavaliauskas5@gmail.com>
2020-04-23 11:32:51 +0300

    THIC-3: Added health to the main character and a health bar which changes color depending on remaining health, fixed player attack script

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public HealthBar1 healthBar;
+
+    void Start() {
+        currentHealth = maxHealth;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            TakeDamage(10);
+        }
+    }
+
+    void TakeDamage(int damage) {
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+    }
+}
--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -2,26 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Attack : MonoBehaviour
+public class PlayerCombat : MonoBehaviour
 {
 
     public Animator animator;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
 
-    // Update is called once per frame
+    public float attackRate = 2f;
+    private float nextAttackTime = 0f;
+    
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if(Time.time >= nextAttackTime && Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Attack_();
+            Attack();
+            nextAttackTime = Time.time + 1f / attackRate;
         }
     }
-    void Attack_()
+    void Attack()
     {
         animator.SetTrigger("attack");
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null)
+        return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
