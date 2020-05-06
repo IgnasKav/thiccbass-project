70f4a686699655ae3e5a480835fb9fbcbc4e4178
IgnasKav <ignas.kavaliauskas5@gmail.com>
2020-05-07 01:10:40 +0300

    no-task: improved consumables

---
+++ Assets/Scripts/Consumables/SpeedOnPickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedOnPickup : MonoBehaviour
+{
+    public float speedBoost = 100f;
+
+    //Paimamas consumable
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject Warrior = GameObject.Find("Warrior");
+        PlayerMovement playerScript = Warrior.GetComponent<PlayerMovement>();
+        playerScript.boosting = true;
+        playerScript.runSpeed = speedBoost;
+
+        Destroy(gameObject);
+    }
+}
