2c2eb295bef9432fc19461d052c070c7b96047b4
rokisas245 <glirokas@gmail.com>
2020-06-03 19:58:05 +0300

    THIC-58: When pressing restart player starts in the checkpoint, with full health

--- Assets/Scripts/Saving/PlayerData.cs
+++ Assets/Scripts/Saving/PlayerData.cs
@@ -6,13 +6,10 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerData
 {
-    public int health;
     public float[] position;
 
     public PlayerData(Player player)
     {
-        health = player.currentHealth;
-
         position = new float[2];
         position[0] = player.transform.position.x - 2;
         position[1] = player.transform.position.y;
